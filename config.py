0f29f662bd04546f90e63a44671f0cb331984f4a
Shravan6767 <shravanmahadik21@gmail.com>
2026-04-10 19:12:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 client = MongoClient("mongodb+srv://shravan67:Gokuandvegeta16@cluster0.qf0m9y0.mongodb.net/?appName=Cluster0")
 
 # create database
-db = client["animal_charity"]
+db = client["animal_charity_2"]
 
 # collections
 contacts_collection = db["contacts"]
